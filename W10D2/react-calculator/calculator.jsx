4d824547bc02c5040110ca8a62006377f5240b85
lisaw97 <lisawen0614@gmail.com>
2019-10-22 13:32:51 -0700

    w10d2

---
+++ W10D2/react-calculator/calculator.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+class Calculator extends React.Component{
+  constructor(props){
+    super(props);
+    //your code here
+    this.state = {
+      result: 0,
+      num1: "",
+      num2: ""
+    }
+    this.setNum1 = this.setNum1.bind(this);
+    this.setNum2 = this.setNum2.bind(this);
+    this.add = this.add.bind(this);
+    this.subtract = this.subtract.bind(this);
+    this.clear = this.clear.bind(this);
+  }
+
+  //your code here
+  setNum1(e) {
+    const num1 = e.target.value ? parseInt(e.target.value) : "";
+    this.setState({num1});
+  }
+
+  setNum2(e) {
+    const num2 = e.target.value ? parseInt(e.target.value) : "";
+    this.setState({ num2 });
+  }
+
+  add(e) {
+    e.preventDefault();
+    const result = this.state.num1 + this.state.num2;
+    this.setState({ result });
+  }
+
+  subtract(e) {
+    e.preventDefault();
+    const result = this.state.num1 - this.state.num2;
+    this.setState({result});
+  }
+
+  clear(e) {
+    e.preventDefault();
+    this.setState({result: 0, num1: "", num2: ""});
+  }
+
+  render(){
+    const {result, num1, num2} = this.state;
+    return (
+      <div>
+        <h1>{result}</h1>
+        <input onChange={this.setNum1} value={num1}/>
+        <input onChange={this.setNum2} value={num2}/>
+        <button onClick={this.add}>+</button>
+        <button onClick={this.subtract}>-</button>
+        <button onClick={this.clear}>clear</button>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
